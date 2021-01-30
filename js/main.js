dade04288353083ea4bbb1480acdcce6209916a7
LastSkywalkerER <maxdr1998@gmail.com>
2021-01-30 11:20:35 +0300

    correction2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 let num = 266219;
 let numMult = 1;
 
-for (let i = 0; i < String(num).length; i++) {
-  numMult *= Number(String(num)[i]);
+num = String(num);
+for (let i = 0; i < num.length; i++) {
+  numMult *= Number(num[i]);
 };
 
 console.log(numMult);
 
-let numPow = numMult ** 3;
-
-console.log(Number(String(numPow).substr(0, 2)));
+console.log(Number(String(numMult ** 3).substr(0, 2)));
 
 
