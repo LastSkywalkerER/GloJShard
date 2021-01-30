db920a85a0aa95cdf6304959ed3b4c01b2cb805d
LastSkywalkerER <maxdr1998@gmail.com>
2021-01-30 09:36:18 +0300

    correction1

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
 let num = 266219;
-let numString = String(num);
-let numArray = numString.split('');
 let numMult = 1;
 
-for (let i = 0; i < numString.length; i++) {
-  numMult *= Number(numArray[i]);
+for (let i = 0; i < String(num).length; i++) {
+  numMult *= Number(String(num)[i]);
 };
 
 console.log(numMult);
 
 let numPow = numMult ** 3;
 
-for (let i = 0; i < 2; i++) {
-  console.log(Number(String(numPow).split('')[i]));
-};
+console.log(Number(String(numPow).substr(0, 2)));
+
 
