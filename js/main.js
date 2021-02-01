90f7226439bebe89b7168c24ee12e937603f6719
LastSkywalkerER <maxdr1998@gmail.com>
2021-02-01 23:18:36 +0300

    lesson3.1

--- js/main.js
+++ js/main.js
@@ -10,4 +10,27 @@ console.log(numMult);
 
 console.log(Number(String(numMult ** 3).substr(0, 2)));
 
+let lang = prompt('ru или en?');
+let month = [
+  ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+];
 
+const reducer = (accumulator, currentValue) => console.log(currentValue);
+
+if (lang === 'ru') {
+  month[0].reduce(reducer, '');
+} else if (lang === 'en') {
+  month[1].reduce(reducer, '');
+}
+
+switch(lang) {
+  case ('ru'):
+    month[0].reduce(reducer, '');
+    break;
+  case ('en'):
+    month[1].reduce(reducer, '');
+    break;
+}
+
+month[(lang === 'ru') ? 0 : 1].reduce(reducer, '');
