020238a1a007fd1e903ead0c0d0738da85e2a5cf
LastSkywalkerER <maxdr1998@gmail.com>
2021-02-02 08:16:05 +0300

    correction1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
-let num = 266219;
-let numMult = 1;
-
-num = String(num);
-for (let i = 0; i < num.length; i++) {
-  numMult *= Number(num[i]);
-};
-
-console.log(numMult);
-
-console.log(Number(String(numMult ** 3).substr(0, 2)));
+'use strict';
 
 let lang = prompt('ru или en?');
 const month = [
