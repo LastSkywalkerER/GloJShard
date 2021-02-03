ee3bbf68d0c9c78aa2c1cf2f4ea4146995d47674
LastSkywalkerER <maxdr1998@gmail.com>
2021-02-03 10:00:33 +0300

    correction1

--- js/main.js
+++ js/main.js
@@ -3,23 +3,11 @@
 function getOneArgument(oneArgument){
   if (typeof oneArgument !== 'string' ) return 'Получена не строка';
 
-  let i = 0;
-
-  while (oneArgument[i] === ' ') {
-    i++;
-  };
-
-  oneArgument = oneArgument.substring(i);
-
-  i = oneArgument.length-1;
-
-  while (oneArgument[i] === ' ') {
-    i--;
-  };
-
-  oneArgument = oneArgument.substring(0, i + 1);
+  oneArgument = oneArgument.trim();
 
   if (oneArgument.length > 30) oneArgument = oneArgument.replace(oneArgument.substring(30), '...');
 
   return oneArgument;
 };
+
+console.log(getOneArgument(prompt('Введите строку')));
