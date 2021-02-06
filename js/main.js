ab86544af323f8640b90450f46c6527f6a219fdc
LastSkywalkerER <maxdr1998@gmail.com>
2021-02-06 12:36:05 +0300

    lesson7

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'.italics(), 'Воскресенье'.italics()],
+  date = new Date();
+
+for (let key in week) {
+  if (key == date.getDay() - 1) {
+    document.write(week[key].bold());
+  } else {
+    document.write(week[key]);
+  }
+  document.write('<br>');
+  console.log(typeof week[key]);
+}
