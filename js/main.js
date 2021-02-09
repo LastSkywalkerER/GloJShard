2439e7e07adf24a7abf1661a8871df024d630189
LastSkywalkerER <maxdr1998@gmail.com>
2021-02-10 00:29:40 +0300

    lesson9

--- js/main.js
+++ js/main.js
@@ -1,14 +1,54 @@
 'use strict';
 
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'.italics(), 'Воскресенье'.italics()],
-  date = new Date();
+function updateTime() {
 
-for (let key in week) {
-  if (key == date.getDay() - 1) {
-    document.write(week[key].bold());
-  } else {
-    document.write(week[key]);
+  const date = new Date(),
+    week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'.italics(), 'Воскресенье'.italics()],
+    month = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
+
+  function declination(number, oneCase, twoCase, threeCase) {
+    number = String(number);
+    switch (number) {
+      case ('11'):
+        return threeCase;
+      case ('12'):
+        return threeCase;
+      case ('13'):
+        return threeCase;
+      case ('14'):
+        return threeCase;
+    }
+    switch (number[number.length - 1]) {
+      case ('1'):
+        return oneCase;
+      case ('2'):
+        return twoCase;
+      case ('3'):
+        return twoCase;
+      case ('4'):
+        return twoCase;
+      default:
+        return threeCase;
+    }
   }
+
+  function addZero(number) {
+    number = String(number);
+    if (number.length === 1) {
+      return '0' + number;
+    }
+    return number;
+  }
+
+  document.write('Сегодня ' + week[date.getDay()] + ', ' + date.getDate() + ' ' + month[date.getMonth()] + ' ' + date.getFullYear() + ' года, ' + date.getHours() + declination(date.getHours(), ' часа ', ' часа ', ' часов ') + date.getMinutes() + declination(date.getMinutes(), ' минута ', ' минуты ', ' минут ') + date.getSeconds() + declination(date.getSeconds(), ' секунда ', ' секунды ', ' секунд '));
+
   document.write('<br>');
-  console.log(typeof week[key]);
+
+  document.write(addZero(date.getDate()) + '.' +
+    addZero(date.getMonth() + 1) + '.' + date.getFullYear() + ' - ' + addZero(date.getHours()) + '.' + addZero(date.getMinutes()) + '.' + addZero(date.getSeconds()));
+
+  document.close();
+
 }
+
+setInterval(updateTime, 1000);
