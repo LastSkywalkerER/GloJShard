b0d9ab1c77dc16a8b74c2b20bca46171e5e38d30
LastSkywalkerER <maxdr1998@gmail.com>
2021-02-16 14:44:03 +0300

    lesson13

--- js/main.js
+++ js/main.js
@@ -1,54 +1,16 @@
 'use strict';
 
-function updateTime() {
+let wrapper = document.querySelector('.wrapper'),
+  colorText = document.querySelector('.color'),
+  change = document.querySelector('.change');
 
-  const date = new Date(),
-    week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'.italics(), 'Воскресенье'.italics()],
-    month = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
-
-  function declination(number, oneCase, twoCase, threeCase) {
-    number = String(number);
-    switch (number) {
-      case ('11'):
-        return threeCase;
-      case ('12'):
-        return threeCase;
-      case ('13'):
-        return threeCase;
-      case ('14'):
-        return threeCase;
-    }
-    switch (number[number.length - 1]) {
-      case ('1'):
-        return oneCase;
-      case ('2'):
-        return twoCase;
-      case ('3'):
-        return twoCase;
-      case ('4'):
-        return twoCase;
-      default:
-        return threeCase;
-    }
-  }
-
-  function addZero(number) {
-    number = String(number);
-    if (number.length === 1) {
-      return '0' + number;
-    }
-    return number;
-  }
-
-  document.write('Сегодня ' + week[date.getDay()] + ', ' + date.getDate() + ' ' + month[date.getMonth()] + ' ' + date.getFullYear() + ' года, ' + date.getHours() + declination(date.getHours(), ' часа ', ' часа ', ' часов ') + date.getMinutes() + declination(date.getMinutes(), ' минута ', ' минуты ', ' минут ') + date.getSeconds() + declination(date.getSeconds(), ' секунда ', ' секунды ', ' секунд '));
-
-  document.write('<br>');
-
-  document.write(addZero(date.getDate()) + '.' +
-    addZero(date.getMonth() + 1) + '.' + date.getFullYear() + ' - ' + addZero(date.getHours()) + '.' + addZero(date.getMinutes()) + '.' + addZero(date.getSeconds()));
-
-  document.close();
-
+function getRandomHex() {
+  return (Math.floor(Math.random() * Math.floor(999999)) + 1000000).toString(16);
 }
 
-setInterval(updateTime, 1000);
+change.addEventListener('click', () => {
+  let color = '#' + getRandomHex();
+  wrapper.style.backgroundColor = color;
+  colorText.textContent = color;
+  change.style.color = color;
+});
