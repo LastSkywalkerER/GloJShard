cd3b4b415d7a54b11ab071c69bc9b3abd7d256c7
LastSkywalkerER <maxdr1998@gmail.com>
2021-03-02 00:44:24 +0300

    complite action buttons

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 'use strict';
 
-const moveObj = () => {
-  const obj = document.querySelector('.wrapper');
 
-  let start = 0;
+
+const moveObj = () => {
+  const obj = document.querySelector('.wrapper'),
+    play = document.querySelector('.play'),
+    reset = document.querySelector('.reset');
+
+  let start = 0,
+    animation = false,
+    direction = 'right';
 
   const moveAnimationRirght = () => {
-    start += 10;
+    start += 5;
+    direction = 'right';
 
     obj.style.transform = `translateX(${start}px)`;
     if (start < 500) {
@@ -17,17 +24,37 @@ const moveObj = () => {
   };
 
   const moveAnimationLeft = () => {
-    start -= 10;
+    start -= 5;
+    direction = 'left';
 
     obj.style.transform = `translateX(${start}px)`;
-    if (start > 0) {
-      window.requestAnimationFrame(moveAnimationLeft);
-    } else {
-      window.requestAnimationFrame(moveAnimationRirght);
+    if (animation) {
+      if (start > 0) {
+        window.requestAnimationFrame(moveAnimationLeft);
+      } else {
+        window.requestAnimationFrame(moveAnimationRirght);
+      }
     }
   };
 
-  window.requestAnimationFrame(moveAnimationRirght);
+  play.addEventListener('click', () => {
+    if (!animation) {
+      animation = true;
+      if (direction === 'right') {
+        window.requestAnimationFrame(moveAnimationRirght);
+      } else {
+        window.requestAnimationFrame(moveAnimationLeft);
+      }
+    } else {
+      animation = false;
+    }
+  });
+
+  reset.addEventListener('click', () => {
+    start = 0;
+    obj.style.transform = `translateX(${start}px)`;
+  });
 };
+
 
 moveObj();
