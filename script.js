0d6636b92ff95badead138a23ad0775557a5b3ca
LastSkywalkerER <maxdr1998@gmail.com>
2021-03-09 18:29:51 +0300

    add calculations

--- script.js
+++ script.js
@@ -1,6 +1,65 @@
 document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
+    class MoneyExchange {
+        constructor(
+            eur,
+            usd,
+            rub
+        ) {
+            this.exchangeValues = {
+                RUB: {
+                    RUB: 1,
+                    EUR: +eur / +rub,
+                    USD: +usd / +rub,
+                },
+                USD: {
+                    RUB: +rub / +usd,
+                    EUR: +eur / +usd,
+                    USD: 1,
+                },
+                EUR: {
+                    RUB: +rub / +eur,
+                    EUR: 1,
+                    USD: +usd / +eur,
+                }
+            };
 
+            this.selectInputCurrency = document.querySelector('#currency-input');
+            this.inputInputCurrency = document.querySelector('.input');
+            this.selectOutputCurrency = document.querySelector('#currency-output');
+            this.inputOutputCurrency = document.querySelector('.output');
+
+            this.fromCurrency = this.selectInputCurrency.value;
+            this.toCurrency = this.selectOutputCurrency.value;
+
+            this.listeners();
+        }
+
+        outputData() {
+            this.inputOutputCurrency.value = this.inputInputCurrency.value * this.exchangeValues[this.fromCurrency][this.toCurrency];
+        }
+
+        listeners() {
+            this.selectInputCurrency.addEventListener('change', () => {
+                this.fromCurrency = this.selectInputCurrency.value;
+                this.outputData();
+            });
+            this.selectOutputCurrency.addEventListener('change', () => {
+                this.toCurrency = this.selectOutputCurrency.value;
+                this.outputData();
+            });
+
+            this.inputInputCurrency.addEventListener('input', this.outputData.bind(this));
+        }
+    }
+
+    const getExchengerApi = () => fetch('https://api.exchangeratesapi.io/latest');
+
+    getExchengerApi()
+        .then(request => request.json())
+        .then(request => {
+            const exchenger = new MoneyExchange(1, request.rates.USD, request.rates.RUB);
+        });
 
 });
